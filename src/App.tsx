8a2a3f2f25f8818fe67319675e334d61e8dea404
Leandro-Suero <ing.leandrosuero@gmail.com>
2020-03-31 19:11:34 -0300

    Basic functionality to add tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState } from "react";
+
+type FormElement = React.FormEvent<HTMLFormElement>; //tipo para el formulario, submit
+interface ITask {
+    //interface que define lo que van a ser cada uno de los task
+    name: string;
+    done: boolean;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [newTask, setNewTask] = useState<string>("");
+    const [tasks, setTasks] = useState<ITask[]>([]);
+
+    const handleSubmit = (e: FormElement) => {
+        e.preventDefault();
+        addTask(newTask);
+        setNewTask("");
+    };
+
+    const addTask = (name: string) => {
+        const newTasks: ITask[] = [...tasks, { name: name, done: false }];
+        setTasks(newTasks);
+    };
+
+    return (
+        <Fragment>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    onChange={e => setNewTask(e.target.value)}
+                    value={newTask}
+                />
+                <button>Save</button>
+            </form>
+            {tasks.map((t: ITask, i: number) => {
+                return <h2 key={i}>{t.name}</h2>;
+            })}
+        </Fragment>
+    );
 }
 
 export default App;
