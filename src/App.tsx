00aa7b85352615e69d1e57031fd7e39011e2c98c
Leandro-Suero <ing.leandrosuero@gmail.com>
2020-03-31 21:22:08 -0300

    Added crud task and basic styling with bootswatch lumen

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from "react";
+import React, { useState } from "react";
 
 type FormElement = React.FormEvent<HTMLFormElement>; //tipo para el formulario, submit
 interface ITask {
@@ -22,20 +22,64 @@ function App() {
         setTasks(newTasks);
     };
 
+    const toggleDoneTask = (i: number) => {
+        const newTasks: ITask[] = [...tasks];
+        newTasks[i].done = !newTasks[i].done;
+        setTasks(newTasks);
+        console.log("toggled");
+    };
+
+    const deleteTask = (i: number) => {
+        let newTasks: ITask[] = tasks.filter((task, index) => index !== i);
+        console.log(newTasks);
+        setTasks(newTasks);
+        console.log(i);
+        console.log(tasks);
+    };
+
     return (
-        <Fragment>
-            <form onSubmit={handleSubmit}>
-                <input
-                    type="text"
-                    onChange={e => setNewTask(e.target.value)}
-                    value={newTask}
-                />
-                <button>Save</button>
-            </form>
-            {tasks.map((t: ITask, i: number) => {
-                return <h2 key={i}>{t.name}</h2>;
-            })}
-        </Fragment>
+        <div className="container p-4">
+            <div className="row">
+                <div className="col-md-6 offset-md-3">
+                    <div className="card">
+                        <div className="card-body">
+                            <form onSubmit={handleSubmit}>
+                                <input
+                                    type="text"
+                                    onChange={e => setNewTask(e.target.value)}
+                                    value={newTask}
+                                    className="form-control"
+                                />
+                                <button
+                                    className="btn btn-success btn-block mt-2"
+                                    autoFocus
+                                >
+                                    Save
+                                </button>
+                            </form>
+                        </div>
+                    </div>
+                    {tasks.map((t: ITask, i: number) => (
+                        <div className="card card-body mt-2" key={i}>
+                            <h2
+                                style={{
+                                    textDecoration: t.done ? "line-through" : ""
+                                }}
+                                onClick={() => toggleDoneTask(i)}
+                            >
+                                {t.name}
+                            </h2>
+                            <button
+                                className="btn btn-danger"
+                                onClick={() => deleteTask(i)}
+                            >
+                                🗑
+                            </button>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
     );
 }
 
